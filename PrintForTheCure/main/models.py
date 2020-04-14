3e440983d7126597bca256d02ffa690cb856c34c
Michaelszeng <michael.zeng.22@bishops.com>
2020-04-14 14:08:38 -0700

    fixed settings.py, main's templates can be found now

--- PrintForTheCure/main/models.py
+++ PrintForTheCure/main/models.py
@@ -3,3 +3,26 @@ from django.db import models
 from django.utils import timezone
 
 # Create your models here.
+class Donor(models.Model):
+    fName = models.CharField(max_length=100)
+    lName = models.CharField(max_length=100)
+    email = models.CharField(max_length=300)
+    address = models.CharField(max_length=500)
+    state = models.CharField(max_length=2)
+    zipcode = models.CharField(max_length=10)
+    registrationDate = models.DateTimeField('date registered')
+
+class Request(models.Model):
+    def __str__(self):
+        return self.post_text
+    fName = models.CharField(max_length=100)
+    lName = models.CharField(max_length=100)
+    email = models.CharField(max_length=300)
+    numPPE = models.IntegerField(default=0)
+    typePPE = models.IntegerField(default = 0)
+    address = models.CharField(max_length=500)
+    state = models.CharField(max_length=2)
+    zipcode = models.CharField(max_length=10)
+    delivDate = models.DateTimeField('date for delivery')
+    orderDate = models.DateTimeField('date ordered')
+    notes = models.CharField(max_length=4000)
--- PrintForTheCure/main/urls.py
+++ PrintForTheCure/main/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-
 from . import views
+
+app_name = "main"
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('catalogue/', views.catalogue, name='home'),
-    path('login/', views.donorLogin, name='home'),
-    path('nearbyRequests/', views.nearbyRequests, name='home'),
-    path('confirmation/', views.confirmClaim, name='home'),
-    path('thankyou/', views.thankYou, name='home'),
+    path('catalogue/', views.catalogue, name='catalogue'),
+    path('login/', views.donorLogin, name='login'),
+    path('nearbyRequests/', views.nearbyRequests, name='nearby requests'),
+    path('confirmation/', views.confirmClaim, name='confirmation'),
+    path('thankyou/', views.thankYou, name='thank you page'),
 ]
