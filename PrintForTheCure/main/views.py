63c8a1849617484fe1619742f2d7847ce9186835
Michaelszeng <michael.zeng.22@bishops.com>
2020-04-14 22:23:48 -0700

    user authentication working

--- PrintForTheCure/main/views.py
+++ PrintForTheCure/main/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.http import HttpResponseRedirect
 from django.template import loader
+# Custom imports added
+# Need timezone for date/time published
+from django.utils import timezone
+# These are needed for user authentication and persistence
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 def home(request):
@@ -19,9 +24,30 @@ def catalogue(request):
     return HttpResponse(template.render(context, request))
 
 def donorLogin(request):
-    template = loader.get_template('main/donorLogin.html')
-    context = {     #all inputs for the html go in these brackets
-
+    if request.POST:
+        # This tests if the form is the log *in* form
+        if 'username' in request.POST.keys():
+            # IF so, try to authentircate
+            user = authenticate(username=request.POST['username'],
+                password=request.POST['password'])
+            if user is not None:
+                # IF success, then use the login function so the session persists.
+                login(request, user)
+            else:
+                pass
+        elif 'logout' in request.POST.keys():
+            # If so, don't need to check anything else, just kill the session.
+            logout(request)
+            print("logged out successfully")
+    # After we check the forms, set a flag for use in the template.
+    if request.user.is_authenticated:
+        template = loader.get_template('main/home.html')
+        print("login successful")
+    else:
+        template = loader.get_template('main/donorLogin.html')
+        print("login unsuccessful")
+    # Find the template
+    context = {
     }
     return HttpResponse(template.render(context, request))
 
